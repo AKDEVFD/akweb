ae952f9369549cfc1200e20ed7929eb358e4f3c4
automataColosal <andres.cedillo@cocolab.mx>
2026-03-03 23:32:32 -0600

    erase backend

--- backend/config/connection.js
+++
@@ -1,7 +0,0 @@
-const { connect, connection } = require('mongoose');
-
-const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/AKWeb';
-
-connect(uri);
-
-module.exports = connection;
--- backend/routes/api/uploadRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const { uploadImage } = require('../../controllers/uploadController')
-
-// POST /upload/image
-router.post('/image', uploadImage)
-
-module.exports = router
